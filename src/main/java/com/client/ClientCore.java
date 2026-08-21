ebbd6026168ca9a8ed6b41e2553067c2ae503980
tewtone66-cmd <tewtone66@gmail.com>
2026-08-22 02:49:55 +0330

    Update ClientCore.java

--- src/main/java/com/client/ClientCore.java
+++ src/main/java/com/client/ClientCore.java
@@ -6,7 +6,7 @@ import com.client.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.KeyMapping;
 import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -19,7 +19,7 @@ public class ClientCore implements ClientModInitializer {
     private static ClientCore instance;
     private ModuleManager moduleManager;
     private ConfigManager configManager;
-    private KeyBinding openGuiKey;
+    private KeyMapping openGuiKey;
 
     @Override
     public void onInitializeClient() {
@@ -33,11 +33,11 @@ public class ClientCore implements ClientModInitializer {
         this.configManager.loadConfig();
 
         try {
-            openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                     "key.client.open_gui",
                     InputConstants.Type.KEYSYM,
                     GLFW.GLFW_KEY_RIGHT_SHIFT,
-                    KeyBinding.Category.MISC
+                    KeyMapping.CATEGORY_MISC
             ));
         } catch (Throwable t) {
             LOGGER.error("Failed to register keybinding cleanly: ", t);
@@ -45,8 +45,8 @@ public class ClientCore implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (openGuiKey != null && client != null) {
-                while (openGuiKey.wasPressed()) {
-                    if (client.currentScreen == null) {
+                while (openGuiKey.consumeClick()) {
+                    if (client.screen == null) {
                         client.setScreen(new ClientScreen());
                     }
                 }
